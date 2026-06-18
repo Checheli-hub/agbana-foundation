2f9b6dcff315ab0f0f66b93062136aacfad80649
checheli <mercifulservanta@gmail.com>
2026-06-18 06:37:08 -0700

    Fix CORS configuration to allow requests and prevent server crash

--- ngo-backend/server.js
+++ ngo-backend/server.js
@@ -44,7 +44,7 @@ app.use(
 
       console.log("Blocked CORS origin:", origin);
 
-      return callback(new Error("Not allowed by CORS"));
+      return callback(null, true);
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
